7a4713f2c47474f1c4ca39475da4ca2eb83356ff
Jared Alford and Stephan Khoury <stephan@>
2016-10-20 13:29:14 -0700

    Complete jQuery branching to complete all possible outcomes. Add images to display at end.

--- js/scripts.js
+++ js/scripts.js
@@ -6,17 +6,52 @@ $(document).ready(function() {
     var q4  = $("input:radio[name=q4]:checked").val();
 
     if (q1 === "kraken" && q2 === "black" && q3 === "summer" && q4 === "boat") {
-    alert("You got Jack Sparrow");
+    alert("You got Jack Sparrow.");
+    $("#jack").show();
   } else if (q1 === "kraken" && q2 === "black" && q3 === "summer") {
-    alert("You got Jack Sparrow");
+    alert("You got Jack Sparrow.");
+    $("#jack").show();
   } else if (q1 === "kraken" && q4 === "boat" && q3 === "summer") {
-    alert("You got Jack Sparrow");
+    alert("You got Jack Sparrow.");
+    $("#jack").show();
   } else if (q2 === "black" && q4 === "boat" && q3 === "summer") {
-    alert("You got Jack Sparrow");
-  }  else if (q2 === "black" && q4 === "boat" && q1 === "kraken") {
-      alert("You got Jack Sparrow");
+    alert("You got Jack Sparrow.");
+    $("#jack").show();
+  } else if (q2 === "black" && q4 === "boat" && q1 === "kraken") {
+    alert("You got Jack Sparrow.");
+    $("#jack").show();
+  } else if (q1 === "none" && q2 === "beige" && q3 === "nochange" && q4 === "walking") {
+    alert("You got Larry David.");
+    $("#larry").show();
+  } else if (q1 === "none" && q2 === "beige" && q3 === "nochange") {
+    alert("You got Larry David.");
+    $("#larry").show();
+  } else if (q1 === "none" && q4 === "walking" && q3 === "nochange") {
+    alert("You got Larry David.");
+    $("#larry").show();
+  } else if (q2 === "beige" && q4 === "walking" && q3 === "nochange") {
+    alert("You got Larry David.");
+    $("#larry").show();
+  } else if (q2 === "beige" && q4 === "walking" && q1 === "none") {
+    alert("You got Larry David.");
+    $("#larry").show();
+  } else if (q1 === "dog" && q2 === "pink" && q3 === "holiday" && q4 === "privatejet") {
+    alert("You got Miley Cyrus.");
+    $("#Miley").show();
+  } else if (q1 === "dog" && q2 === "pink" && q3 === "holiday") {
+    alert("You got Miley Cyrus.");
+    $("#Miley").show();
+  } else if (q1 === "dog" && q4 === "privatejet" && q3 === "holiday") {
+    alert("You got Miley Cyrus.");
+    $("#Miley").show();
+  } else if (q2 === "pink" && q4 === "privatejet" && q3 === "holiday") {
+    alert("You got Miley Cyrus.");
+    $("#Miley").show();
+  } else if (q2 === "pink" && q4 === "privatejet" && q1 === "dog") {
+    alert("You got Miley Cyrus.");
+    $("#Miley").show();
   } else {
-    alert("you didn't get Jack")
+    alert("You don't have a clear preference.");
   }
 
   event.preventDefault();
